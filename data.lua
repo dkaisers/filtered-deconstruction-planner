100d61a3ba423c9cda12299034471ee7eefec819
Dominik Kaisers <dominik@dkaisers.net>
2016-07-10 23:32:00 +0200

    Removed item itself as configuration now affects the default deconstruction planner

--- data.lua
+++ data.lua
@@ -1,4 +1,1 @@
-require "prototypes.item"
-require "prototypes.recipe"
-require "prototypes.technology"
 require "prototypes.style"
--- prototypes/technology.lua
+++
@@ -1,4 +0,0 @@
-table.insert(
-  data.raw["technology"]["automated-construction"]["effects"],
-  {type = "unlock-recipe", recipe = "filtered-deconstruction-planner"}
-)
